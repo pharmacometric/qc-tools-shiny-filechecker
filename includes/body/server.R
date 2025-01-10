d1e6cdb2af59df14f64c2a77ab0501f538386df4
William Hane <162056037+pharmacometric@users.noreply.github.com>
2025-01-09 23:22:48 -0500

    Update server.R

--- includes/body/server.R
+++ includes/body/server.R
@@ -29,31 +29,3 @@ observe({
 
 
 
-
-observeEvent(input$dirfiletype1a,{
-  fillisttxt = c()
-  if(!dir.exists(input$dirfiletype1a)) {
-    updateDirStatus("Directory does not exist for Original")
-    shinyjs::runjs(paste0("$('#dirfiletype1afiles').html(\'\')"))
-   return("")
-  }
-  for(kfil in sort(list.files(path = input$dirfiletype1a))){
-    fillisttxt = c(fillisttxt,paste0('<div class="filelistc1"><i class="fas fa-file-pdf" role="presentation" aria-label="chart-area icon"></i> ',
-                                     kfil,'</div>'))
-  }
-  shinyjs::runjs(paste0("$('#dirfiletype1afiles').html(\'",paste(fillisttxt,collapse = ""),"\')"))
-})
-
-observeEvent(input$dirfiletype1b,{
-  fillisttxt = c()
-  if(!dir.exists(input$dirfiletype1b)) {
-    updateDirStatus("Directory does not exist for QC")
-    shinyjs::runjs(paste0("$('#dirfiletype1bfiles').html(\'\')"))
-    return("")
-  }
-  for(kfil in sort(list.files(path = input$dirfiletype1b))){
-    fillisttxt = c(fillisttxt,paste0('<div class="filelistc1"><i class="fa-regular fa-file-excel"></i> <i class="fa-solid fa-file-word"></i> ',
-                                     kfil,'</div>'))
-  }
-  shinyjs::runjs(paste0("$('#dirfiletype1bfiles').html(\'",paste(fillisttxt,collapse = ""),"\')"))
-})

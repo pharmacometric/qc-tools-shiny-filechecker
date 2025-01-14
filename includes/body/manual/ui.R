60acd7de02f8e18e2957dbd3cc7a7a4de2d0fef2
William Hane <162056037+pharmacometric@users.noreply.github.com>
2025-01-14 15:53:21 -0500

    Create ui.R

---
+++ includes/body/manual/ui.R
@@ -0,0 +1,21 @@
+############################################################################
+############################################################################
+##  Document Path: includes/body/ui.R
+##
+##  Description: User interface for main body section
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+body.body.manual = card.pro(
+  title = "Guide for using the QC file checker", width = 12,
+  header.bg = "darken",
+  removebtn = FALSE,
+  colorbtn = FALSE,
+  expandbtn = FALSE,
+  editbtn = FALSE,
+  collapsed = 1L,
+  shadow = FALSE,
+  shiny::includeMarkdown(file.path(this.path, "model_information.md"))
+)

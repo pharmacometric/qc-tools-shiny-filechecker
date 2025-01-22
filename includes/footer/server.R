e5e553aad3c703f2aa890a1cd15b4ba1ace5fc1e
William Hane <162056037+pharmacometric@users.noreply.github.com>
2025-01-21 22:30:07 -0500

    Update server.R

--- includes/footer/server.R
+++ includes/footer/server.R
@@ -1,1 +1,15 @@
-# include any server functions for footer
+############################################################################
+############################################################################
+##  Document Path: includes/footer/server.R
+##
+##  Description: server for footer
+##
+##  R Version: R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+
+# when cookie is clicked
+observeEvent(input$closecookiesform,{
+  shinyjs::runjs("document.querySelectorAll('.page-footer').forEach(element => element.remove())")
+})
